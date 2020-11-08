fe5f1abea69b1596e6049e4efc21d766d02f0e2f
Vatthanachai Wongprasert <vatthanachai.w@gmail.com>
2020-11-08 20:40:35 +0700

    Update Todo Controller for V2.0

---
+++ src/Todo.API/Controllers/V2_0/TodoController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Todo.API.Controllers.V2_0
+{
+    /// <summary>
+    /// Todo API Controller
+    /// </summary>
+    [ApiController]
+    [ApiVersion("2.0")]
+    [Produces("application/json")]
+    [Route("api/v{version:apiVersion}/[controller]/[action]")]
+    public class TodoController
+    {
+        /// <summary>
+        /// Method นี้ยังไม่ได้เขียนโค๊ดเลยนะ 
+        /// </summary>
+        /// <returns>NotImplementedException</returns>
+        [HttpGet]
+        public async Task<IActionResult> GetAllAsync()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
